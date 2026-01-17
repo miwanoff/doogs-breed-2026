9b24a47599033ba1ab77267a02a48712378d5380
miwanoff <marina.iva.noffa@gmail.com>
2026-01-17 13:03:31 +0200

    Add onmousemove

--- script.js
+++ script.js
@@ -12,4 +12,36 @@ for (let i = 0; i < irises.length; i++) {
 function go(event) {
   console.log(event.target.id);
   const flower = document.getElementById(event.target.id);
+  const breed = flower.dataset.breed;
+  console.log(breed);
+  const coords = getCoords(flower);
+  const shiftX = event.pageX - coords.left;
+  const shiftY = event.pageY - coords.top;
+  console.log(shiftX, shiftY);
+
+  function moveAt(event) {
+    // сдвиг курсора на shiftX и shiftY относительно верхнего левог7о угла
+    const left = event.pageX - shiftX;
+    const top = event.pageY - shiftY;
+    flower.style.left = left + "px";
+    flower.style.top = top + "px";
+    showCoords.innerHTML = `x: ${left} y ${top}`;
+  }
+
+  flower.ondragstart = function () {
+    return false; // отмена drag and drop браузера
+  };
+
+  document.onmousemove = function (e) {
+    moveAt(e);
+  };
+}
+
+function getCoords(elem) {
+  //getBoundingClientRect()  функция возвращает размер элемента и его координаты относительно объемлющего элемента.
+  var box = elem.getBoundingClientRect();
+  return {
+    top: box.top,
+    left: box.left,
+  };
 }
