7d1c298ab686679083a45eafd79c1046fcc2c18e
miwanoff <marina.iva.noffa@gmail.com>
2026-01-17 12:49:50 +0200

    Add go

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const tan = document.getElementById("tan");
 const pink = document.getElementById("pink");
+const showCoords = document.getElementById("showCoords");
+const state = document.getElementById("state");
 
+const irises = document.getElementsByClassName("iris");
+
+for (let i = 0; i < irises.length; i++) {
+  irises[i].addEventListener("mousedown", go);
+}
+
+function go(event) {
+  console.log(event.target.id);
+  const flower = document.getElementById(event.target.id);
+}
